bba22dbfcc8f9931e03ae57991fe0b4a9bdd004e
S <ace.king@live.cn>
2017-09-11 10:56:06 +0800

    new start

---
+++ src/component/header.jsx
@@ -0,0 +1,19 @@
+import React, {Component} from 'react';
+import  './header.css'
+
+class Header extends Component {
+
+    render() {
+        return (
+            <div>
+                <header className={'page-wrapper'}>
+                    <div className={'ant_row'}>
+
+                    </div>
+                </header>
+            </div>
+        );
+    }
+}
+
+export default Header;
